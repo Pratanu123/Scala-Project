a354bd5b907c22b464b2c7d2a8e170087d924d28
Pratanu Khajanchi <pratanukhajanchi@gmail.com>
2018-08-17 17:24:13 +0530

    Update chatMessage.scala

--- Scala Project/src/main/scala/chatMessage.scala
+++ Scala Project/src/main/scala/chatMessage.scala
@@ -14,17 +14,31 @@ object chatMessage extends App {
 
 class Ping(pong: ActorRef) extends Actor {
   var count = 0
+  lazy val x = (1 to 50).toList
+  val m = Map(1 -> "A", 2 -> "B", 3 -> "C", 4 -> "D", 5 -> "E")
+
   def incrementAndPrint {
     count += 1;
     println("ping")
+    println("Sum is " + x.reduce(_ + _))
+    println(m.get(count))
+    if(count%2==0){
+      println("Even numbers are- "+x.filter(_%2==0))
+    }
+    else {
+
+      println("Using fold left sum- "+x.foldLeft(0)(_+_))
+    }
+
   }
+
   def receive = {
     case StartMessage =>
       incrementAndPrint
       pong ! PingMessage
     case PongMessage =>
       incrementAndPrint
-      if (count > 30) {
+      if (count > 5) {
         sender ! StopMessage
         println("ping stopped")
         context.stop(self)
